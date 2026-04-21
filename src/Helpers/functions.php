e2f49b0b9ac8dbd528bb08104b778dbfd2a07836
Julius <julius.derigs@mail.de>
2026-04-21 11:05:33 +0200

    Feature: Implemented a general helper.

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -1,17 +1,65 @@
 <?php
 
 use Pangio\Core\System\Config;
+use Pangio\Core\Http\Router;
 
 if (!function_exists('baseURL')) {
     /**
      * Builds a URL out of the config baseURL and the given URI and returns it.
      *
-     * @param string $uri
+     * @param string $path
      * @return string
      */
-    function baseURL(string $uri = '') :string {
-        $baseURL = $_ENV['BASE_URL'] ?? Config::get('app.baseURL');
+    function baseURL(string $path = ''): string {
+        return Router::baseURL($path);
+    }
+}
 
-        return $baseURL . $uri;
+if (!function_exists('trans')) {
+    /**
+     * @param string $key
+     * @param array $replace
+     * @return string
+     */
+    function trans(string $key, array $replace = []) :string {
+        $locale = $_ENV['APP_LOCALE'] ?? Config::get('app.locale') ?? 'de';
+
+        static $lines = [];
+
+        if (!isset($lines[$locale])) {
+            $path = __DIR__ . "/../Lang/{$locale}.php";
+            $path = dirname(__DIR__, 2) . "/app/Lang/$locale.php";
+
+            $lines[$locale] = file_exists($path)
+                ? require $path
+                : [];
+        }
+
+        $value = dataGet($lines[$locale], $key, $key);
+
+        foreach ($replace as $search => $replaceValue) {
+            $value = str_replace(":$search", $replaceValue, $value);
+        }
+
+        return $value;
+    }
+}
+
+if (!function_exists('dataGet')) {
+    /**
+     * @param array $array
+     * @param string $key
+     * @param mixed|null $default
+     * @return mixed
+     */
+    function dataGet(array $array, string $key, mixed $default = null) :mixed {
+        foreach (explode('.', $key) as $segment) {
+            if (!is_array($array) || !array_key_exists($segment, $array)) {
+                return $default;
+            }
+            $array = $array[$segment];
+        }
+
+        return $array;
     }
 }
