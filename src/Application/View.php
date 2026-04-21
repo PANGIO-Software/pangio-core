30b026abc4341ae34007f484449aab271a0e9423
Julius <julius.derigs@mail.de>
2026-04-21 11:02:01 +0200

    Feature: Implemented the View class.

---
+++ src/Application/View.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types = 1);
+
+namespace Pangio\Core\Application;
+
+use RuntimeException;
+
+class View {
+    /**
+     * Renders a view file with optional parameters and returns the generated output as a string.
+     *
+     * @param string $view
+     * @param array $params
+     * @return string
+     */
+    public function render(string $view, array $params = []): string {
+        $viewDirectory = dirname(__DIR__, 2) . '/views';
+        $viewPath = $viewDirectory . '/' . $view . '.php';
+
+        if (!is_file($viewPath)) {
+            throw new RuntimeException("View not found: $viewPath");
+        }
+
+        extract($params, EXTR_SKIP);
+
+        ob_start();
+        include $viewPath;
+
+        $content = ob_get_clean();
+
+        return $content !== false ? $content : '';
+    }
+}
