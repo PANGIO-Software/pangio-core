82951ce40aaf6574e9160a3a563a78a4bde1cecb
Julius Derigs <julius.derigs@mail.de>
2026-05-03 13:07:56 +0200

    Made Pangio\Core\Application\View static and added PHPDocs comments.

--- src/Application/View.php
+++ src/Application/View.php
@@ -6,7 +6,18 @@ namespace Pangio\Core\Application;
 use Pangio\Core\System\Session;
 use RuntimeException;
 
+/**
+ * Provides a static utility for rendering PHP view templates with injected parameters and session-based flash messages,
+ * returning the generated output as a string.
+ *
+ * @author Julius Derigs <julius.derigs@pangio.de>
+ */
+
 class View {
+    ####################################################################################################################
+    # --- PUBLIC METHODS --------------------------------------------------------------------------------------------- #
+    ####################################################################################################################
+
     /**
      * Renders a view file with optional parameters and returns the generated output as a string.
      *
@@ -14,7 +25,7 @@ class View {
      * @param array $params
      * @return string
      */
-    public function render(string $view, array $params = []): string {
+    public static function render(string $view, array $params = []): string {
         $viewDirectory = dirname(__DIR__, 2) . '/app/Views/';
         $viewPath = $viewDirectory . '/' . $view . '.php';
 
